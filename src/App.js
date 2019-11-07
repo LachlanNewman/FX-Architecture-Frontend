1b580a0309931c030247590060ac4feff7686d11
Lachlan <lachnewman007@gmail.com>
2019-11-07 23:58:41 +1100

    Add Favicon

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ import {Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom'
 import Home from "./components/Home/Home";
 import Renders from "./components/Renders/Renders";
 
-const scrollToRef = (ref) => {ref.current.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-})}   // General scroll to element function
-
-
 const App = () => {
-
-    const rendersRef = React.createRef();
 
     return (
         <div>
@@ -25,8 +17,8 @@ const App = () => {
                             <li className={'navigation__item navigation__item--yellow'}>
                                 <Link to={'/'}>Home</Link>
                             </li>
-                            <li onMouseEnter={() => scrollToRef(rendersRef)} className={'navigation__item'}>
-                                <Link to={'/'}>Renders</Link>
+                            <li className={'navigation__item'}>
+                                <Link to={'/renderings'}>Renders</Link>
                             </li>
                             <li className={'navigation__item navigation__item--yellow'}>
                                 <Link to={'/'}>Link 2</Link>
@@ -39,13 +31,10 @@ const App = () => {
 
                     <Switch>
                         <Route exact path={"/"} component={Home}/>
-                        {/*<Route exact path={"/renderings"} component={Renderings}/>*/}
+                        <Route exact path={"/renderings"} component={Renders}/>
                     </Switch>
                 </div>
             </Router>
-            <section id={'renders'} ref={rendersRef}>
-                <Renders/>
-            </section>
         </div>
     )
 }
--- src/components/Renders/Renders.js
+++ src/components/Renders/Renders.js
@@ -3,11 +3,13 @@ import React from "react";
 const Renders = () => {
     return (
         <div className={'renders'}>
-            <h2>Renders</h2>
-            <div className={'renders__content'}>
-                <div className={'renders__text'}></div>
-                <div className={'renders__image'}>
-                    <img src={process.env.PUBLIC_URL + "imgs/gimnasio.jpg"}/>
+            <div className={'renders__container'}>
+                <h2>Renders</h2>
+                <div className={'renders__content'}>
+                    <div className={'renders__text'}></div>
+                    <div className={'renders__image'}>
+                        <img src={process.env.PUBLIC_URL + "imgs/gimnasio.jpg"}/>
+                    </div>
                 </div>
             </div>
         </div>
